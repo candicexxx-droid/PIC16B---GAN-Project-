2c88b9331159f5dd4e092a78218ffc4b0bc996c3
Sylviama1026 <59939345+Sylviama1026@users.noreply.github.com>
2021-06-03 14:06:01 +0800

    Fix error in gif

--- GIF_training.py
+++ GIF_training.py
@@ -6,11 +6,16 @@
 
 import glob
 import imageio
+import re
+import pandas as pd
 anim_file = 'gan.gif'
 
 with imageio.get_writer(anim_file, mode='I') as writer:
     filenames = glob.glob('Epoch_*.png')
-    filenames = sorted(filenames)
+    df=pd.DataFrame({'filenames':filenames})
+    df['num']=df.filenames.apply(lambda x: re.findall(r"[0-9]+", x)[0]).astype(int)
+    df=df.sort_values(by=['num'])
+    filenames=df.filenames
     for filename in filenames:
         image = imageio.imread(filename)
         writer.append_data(image)
